9cb6b52289abedf61b839f0de1424baa4ccce814
Riku <poprestinhim@gmail.com>
2025-05-27 10:45:54 +0900

    chore: remove unused files and configurations to streamline the project structure, including .gitignore, package.json, and various components

--- web/src/assets/icons/index.ts
+++
@@ -1,6 +0,0 @@
-import ArrowLeft from "./arrow-left.svg";
-import HomeIcon from "./home-icon.svg";
-import ChevronRight from "./chevron-right.svg";
-import InfoCircle from "./info-circle.svg";
-
-export { ArrowLeft, HomeIcon, ChevronRight, InfoCircle };
